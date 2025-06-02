baff0bb6daaa41ce8e6811fbb62afa6c2f3e2a5c
Atai Chynaliev <ataichynaliev@Atais-MacBook-Pro.local>
2025-06-03 02:25:48 +0600

    rework

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import HeroSection from '../components/HeroSection';
+import ServicesSection from '../components/ServicesSection';
 import AudienceSection from '../components/AudienceSection';
-import CatalogSection from '../components/CatalogSection';
 import GallerySection from '../components/GallerySection';
 import FeaturesSection from '../components/FeaturesSection';
 import ContactForm from '../components/ContactForm';
@@ -9,8 +9,8 @@ const HomePage = () => {
   return (
     <div className="bg-white">
       <HeroSection />
+      <ServicesSection />
       <AudienceSection />
-      <CatalogSection />
       <GallerySection />
       <FeaturesSection />
       <ContactForm />
