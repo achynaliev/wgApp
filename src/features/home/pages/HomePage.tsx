8f393aff7a50c9a3a41fb39711ef9c9c7a4425cf
Atai Chynaliev <ataichynaliev@Atais-MacBook-Pro.local>
2025-06-03 02:23:02 +0600

    removed a section

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import HeroSection from '../components/HeroSection';
-import ServicesSection from '../components/ServicesSection';
 import AudienceSection from '../components/AudienceSection';
 import CatalogSection from '../components/CatalogSection';
 import GallerySection from '../components/GallerySection';
@@ -10,7 +9,6 @@ const HomePage = () => {
   return (
     <div className="bg-white">
       <HeroSection />
-      <ServicesSection />
       <AudienceSection />
       <CatalogSection />
       <GallerySection />
