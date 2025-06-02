ebc15f26c29b2c3e3f0e214486bcc7923bd1db12
Atai Chynaliev <ataichynaliev@Atais-MacBook-Pro.local>
2025-06-03 02:04:08 +0600

    rollback home screen changes

--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,16 +1,20 @@
 import HeroSection from '../components/HeroSection';
-import AdvantagesSection from '../components/AdvantagesSection';
+import ServicesSection from '../components/ServicesSection';
+import AudienceSection from '../components/AudienceSection';
 import CatalogSection from '../components/CatalogSection';
 import GallerySection from '../components/GallerySection';
+import FeaturesSection from '../components/FeaturesSection';
 import ContactForm from '../components/ContactForm';
 
 const HomePage = () => {
   return (
     <div className="bg-white">
       <HeroSection />
-      <AdvantagesSection />
+      <ServicesSection />
+      <AudienceSection />
       <CatalogSection />
       <GallerySection />
+      <FeaturesSection />
       <ContactForm />
     </div>
   );
